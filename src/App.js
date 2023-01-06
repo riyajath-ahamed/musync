13c8aca8f3367eadedfde22e9deef8cad4fe0de2
riyajath-ahamed <riyajatha@gmail.com>
2023-01-06 19:18:29 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Home, Login } from './components'
+import { app } from './config/firebase.config'
+import { getAuth } from 'firebase/auth'
+
+const App = () => {
+
+  const firbaseAuth = getAuth(app);
+  const navigate = useNavigate();
+
+
+
+  const [auth, setAuth] = useState(false || window.localStorage.getItem ("auth") === "true")
+
+  useEffect(() => {
+    firbaseAuth.onAuthStateChanged((userCred)=>{
+      //console.log(userCred)
+      if(userCred){
+        userCred.getIdToken().then((token) =>{
+          console.log(token);
+        })
+      }else{
+        setAuth(false);
+        window.localStorage.setItem("auth", "false");
+        navigate("/login")
+      }
+    })
+
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-screen h-screen bg-primary flex justify-center items-center'>
+      <Routes>
+        <Route path='/login'  element={<Login setAuth={setAuth}/>}/>
+        <Route path='/*' element={<Home/>}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { app } from "../config/firebase.config";
+import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+
+
+import {FcGoogle} from 'react-icons/fc';
+import bg1 from '../Asset/bg1.mp4';
+import { useEffect } from 'react';
+
+const Login = ({setAuth}) => {
+
+
+  const firbaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const navigate = useNavigate();
+
+  const loginWithGoogle = async() => {
+    console.log('login with google')
+
+    await signInWithPopup(firbaseAuth, provider).then((userCred) => {
+      //console.log(userCred);
+      if (userCred) {
+        setAuth(true);
+        window.localStorage.setItem('auth', "true");
+        firbaseAuth.onAuthStateChanged((userCred)=>{
+          if (userCred) {
+            //console.log(userCred);
+            userCred.getIdToken().then((token) =>{
+              console.log(token);
+            })
+            navigate("/", {replace : true})
+          }
+          else{
+            setAuth(false);
+            navigate("/login")
+          }
+        })
+      }
+    }) 
+
+
+
+  };
+
+//session handling Login session and redirect to home page or login page
+
+  useEffect(( ) => {
+    if(window.localStorage.getItem('auth') === "true"){
+      navigate("/", {replace : true})
+    }
+  }, [])
+  
+  return (
+    <div className='relative w-screen h-screen'>
+      <video
+        src={bg1}
+        type="video/mp4"
+        autoPlay
+        muted
+        loop
+        className="w-full h-full object-cover"
+      ></video>
+      <div className='absolute inset-0 bg-darkOverlay flex items-center justify-center p-4'>
+        <div className='w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex-col items-center justify-center'>
+          <div className="bg-white rounded-lg overflow-hidden mb-5 shadow-2xl">
+            <div className="group p-8 hover:bg-orange-400 hover:shadow-md duration-100 ease-in-out transition-all" >
+              <div className='font-bold group-hover:text-white'>Musync</div>
+              
+            </div>
+          </div>
+          <div>
+            <div className="bg-white rounded-lg overflow-hidden shadow-2xl">
+                <div className="p-8">
+                <label for="email" className="block mb-2 text-2xl font-medium text-black">Login</label>
+                    <form method="POST" className="" action="#" onsubmit="return false;">
+                        <div className="mb-5">
+                            <label for="email" className="block mb-2 text-sm font-medium text-gray-600">Email</label>
+                            <input type="text" name="email" className="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none" />
+                        </div>
+                        <div className="mb-5">
+                            <label for="password" className="block mb-2 text-sm font-medium text-gray-600">Password</label>
+                            <input type="password" name="password" className="block w-full p-3 rounded bg-gray-200 border border-transparent focus:outline-none" />
+                        </div>
+                        <button className="w-full p-3 mt-4 bg-indigo-600 text-white rounded shadow">Login</button>
+                    </form>
+                </div>
+                <div className="flex justify-between p-8 text-sm border-t border-gray-300 bg-gray-100">
+                    <a className="font-medium text-indigo-500">Create account</a>
+                    <a className="text-gray-600">Forgot password?</a>
+                </div>
+            </div>
+            
+          </div>
+          <div 
+          onClick={loginWithGoogle} 
+          className='flex items-center justify-center gap-2 px-4 py-2 mt-5 rounded-md bg-cardOverlay cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all'
+          
+          >
+            <FcGoogle className='text-xl'/>Sign in with Google
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Login;
