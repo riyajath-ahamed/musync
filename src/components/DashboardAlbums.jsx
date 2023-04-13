594f19961767a9966ab9983425caa5427e3391db
riyajath-ahamed <riyajatha@gmail.com>
2023-04-13 15:06:51 +0530

    dashboard Album & Artist

--- src/components/DashboardAlbums.jsx
+++ src/components/DashboardAlbums.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
+import { getAllAlbums } from '../api';
+import SongCard from './SongCard';
 
 const DashboardAlbums = () => {
+
+  const [{allAlbums}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //DATA. DATA ERROR IN USEEFFECT
+    if(!allAlbums){
+      getAllAlbums().then((data) => {
+        dispatch({
+          type: actionType.SET_ALL_ALBUMS,
+          all: data.data,
+        });
+        
+      })
+    }
+
+  }, [])
+
   return (
-    <div>DashboardAlbums</div>
+    <div>
+      <div className='w-full p-4 flex items-center justify-center flex-col'>
+      
+      <div className="relative w-full my-4 p-4 pt-14 border-2 border-orange-200 rounded-md">
+        
+        <AlbumContainer data={allAlbums}/>
+
+      </div>
+
+    </div>
+    </div>
   )
+}
+
+
+export const AlbumContainer = ({data}) => {
+  return(
+    <div className="w-full flex flex-wrap gap-3 items-center justify-evenly">
+      {data && data.map((song,i) => (
+        <SongCard key={song._id}
+        data={song} index={i}
+        />
+      ))}
+
+
+    </div>
+  )
+    
+  
 }
 
 export default DashboardAlbums
--- src/components/DashboardArtists.jsx
+++ src/components/DashboardArtists.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useStateValue } from '../context/StateProvider';
+import { getAllArtist } from '../api';
+import { actionType } from '../context/reducer';
+import SongCard from './SongCard';
 
 const DashboardArtists = () => {
+
+  const [{allArtists}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //DATA. DATA ERROR IN USEEFFECT
+    if(!allArtists){
+      getAllArtist().then((data) => {
+        dispatch({
+          type: actionType.SET_ALL_ARTISTS,
+          allArtists: data.data,
+        });
+        
+      })
+    }
+
+  }, [])
+
   return (
-    <div>DashboardArtist</div>
+    <div className='w-full p-4 flex items-center justify-center flex-col'>
+      
+      <div className="relative w-full my-4 p-4 pt-14 border-2 border-orange-200 rounded-md">
+        
+        <ArtistContainer data={allArtists}/>
+
+      </div>
+
+    </div>
   )
 }
+
+export const ArtistContainer = ({data}) => {
+  return(
+    <div className="w-full flex flex-wrap gap-3 items-center justify-evenly">
+      {data && data.map((song,i) => (
+        <SongCard key={song._id}
+        data={song} index={i}
+        />
+      ))}
+
+
+    </div>
+  )
+    
+  
+}
+
 
 export default DashboardArtists
