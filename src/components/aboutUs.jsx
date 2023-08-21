a558dda22169d470926b1c91dcf1519091c330f4
riyajath-ahamed <riyajatha@gmail.com>
2023-08-21 21:47:37 +0530

    About us |

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import { CgUserlane } from "react-icons/cg"
 import { IoSpeedometer } from 'react-icons/io5'
 import { SiServerless } from 'react-icons/si'
+import { BsGithub } from 'react-icons/bs'
 import logo from "../Asset/logo4.png"
 
 const aboutus = () => {
@@ -86,13 +87,13 @@ const aboutus = () => {
                     <p className="font-bold text-orange-600">
                         MuSync. <br /> Emotion Delection Partner
                     </p>
-                    <p className='text-orange-500'>Copyright © 2023 - All right reserved</p>
+                    <p className='text-orange-500'> © 2023. All rights reserved -<span className='block lg:inline-block'> Made with<span className='inline-block h-4'>{heart}</span> by<a href='https://github.com/riyajath-ahamed'> Riyajath Ahamed</a></span></p>
                 </div>
                 <div>
                     <div className="grid grid-flow-col gap-4 text-orange-600">
-                        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-                        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-                        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+                        <a href='https://github.com/riyajath-ahamed/musync'>
+                        <BsGithub className='w-6 h-6'/>
+                        </a>
                     </div>
                 </div>
             </footer>
@@ -102,3 +103,7 @@ const aboutus = () => {
 }
 
 export default aboutus
+
+export const heart = <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="0" stroke="currentColor" class="w-5 h-5">
+<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+</svg>
