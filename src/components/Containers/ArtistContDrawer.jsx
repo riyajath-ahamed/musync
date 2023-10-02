ea3136e04abfe883de37a4ab79cb24b7190c6b9a
riyajath-ahamed <riyajatha@gmail.com>
2023-10-02 19:17:22 +0530

    Library Handeling | Dashboard Handeling

---
+++ src/components/Containers/ArtistContDrawer.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import SongCard from '../SongCard'
+import { useStateValue } from '../../context/StateProvider';
+import { getAllArtist } from '../../api';
+import { actionType } from '../../context/reducer';
+
+const ArtistContDrawer = () => {
+
+    const [{allArtists}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //DATA. DATA ERROR IN USEEFFECT
+    if(!allArtists){
+      getAllArtist().then((data) => {
+        dispatch({
+          type: actionType.SET_ALL_ARTISTS,
+          allArtists: data.data,
+        });
+        
+      })
+    }
+
+  }, [])
+
+
+  return (
+    <div>
+        <div className="w-full flex flex-wrap gap-3 items-center justify-evenly">
+    {allArtists && allArtists.map((song,i) => (
+      <SongCard key={song._id}
+      data={song} index={i}
+      type="artist"
+      />
+    ))}
+
+
+  </div></div>
+  )
+}
+
+export default ArtistContDrawer
